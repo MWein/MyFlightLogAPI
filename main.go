792f4964fe7c7fbb4747683ac411e53df741051a
unknown <mweinberg21@gmail.com>
2020-07-12 02:45:56 -0500

    Added plane image endpoint

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func handleRequests() {
 
 	http.HandleFunc("/ping", endpoints.Ping)
 	http.HandleFunc("/log", endpoints.FlightLogs)
+	http.HandleFunc("/plane-image", endpoints.PlaneImg)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
---
+++ src/endpoints/planeImg.go
@@ -0,0 +1,41 @@
+package endpoints
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+)
+
+func PlaneImg(w http.ResponseWriter, r *http.Request) {
+
+	idents, ok := r.URL.Query()["ident"]
+
+	if !ok || len(idents[0]) < 1 {
+		fmt.Fprintf(w, "ident is required")
+		return
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	planeImgStatement := `SELECT pic FROM plane WHERE ident = $1`
+
+	var image []byte
+	err = db.QueryRow(planeImgStatement, idents[0]).Scan(&image)
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.Write(image)
+}
