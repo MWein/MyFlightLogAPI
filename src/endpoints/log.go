12e17bf70137185e13085f07c0dbfe5cc554633d
unknown <mweinberg21@gmail.com>
2020-07-12 19:20:47 -0500

    Moved psqlInfo to own file

--- src/endpoints/log.go
+++ src/endpoints/log.go
@@ -54,14 +54,6 @@ type LogsReturn struct {
 
 
 
-// Yeah, yeah, don't commit secrets. Its a local DB for now so I don't care
-const (
-	host = "localhost"
-	port = 5432
-	user = "postgres"
-	password = "123"
-	dbname = "MyFlightLog"
-)
 
 
 func FlightLogs(w http.ResponseWriter, r *http.Request) {
