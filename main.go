861f08317c2782e46fb9e0c52677b0c9a30dbb15
unknown <mweinberg21@gmail.com>
2020-07-12 22:25:53 -0500

    Removed placeholder functions

--- main.go
+++ main.go
@@ -8,15 +8,6 @@ import (
 )
 
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
-}
-
-func otherPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Other Page Endpoint Hit")
-}
-
-
 func handleRequests() {
 	http.HandleFunc("/log", endpoints.FlightLogs)
 	http.HandleFunc("/plane-image", endpoints.PlaneImg)
