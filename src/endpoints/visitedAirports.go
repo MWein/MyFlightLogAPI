d9d9e0ded4d90fb07f46676ad1af7425b95213fc
unknown <mweinberg21@gmail.com>
2020-07-22 01:11:12 -0500

    Added last visited date to airports

--- src/endpoints/visitedAirports.go
+++ src/endpoints/visitedAirports.go
@@ -12,17 +12,22 @@ type Airport struct {
 	Name  string  `json:"name"`
 	Lat   float64 `json:"lat"`
 	Long  float64 `json:"long"`
+	LastVisited string `json:"lastVisited"`
 }
 
 type Airports []Airport
 
 func VisitedAirports(w http.ResponseWriter, r *http.Request) {
-	rows, _ := database.DBConnection.Query("SELECT ident, name, lat, long FROM (SELECT unnest(stops) FROM log GROUP BY unnest) AS x JOIN airport ON ident = unnest")
+	rows, _ := database.DBConnection.Query("SELECT ident, name, lat, long, (SELECT date FROM log WHERE airport.ident = ANY(stops) ORDER BY date DESC LIMIT 1) FROM (SELECT unnest(stops) FROM log GROUP BY unnest) AS x JOIN airport ON ident = unnest")
 
 	var airports Airports
 	for rows.Next() {
 		var airport Airport
-		rows.Scan(&airport.Ident, &airport.Name, &airport.Lat, &airport.Long)
+		rows.Scan(&airport.Ident, &airport.Name, &airport.Lat, &airport.Long, &airport.LastVisited)
+
+		// Remove timestamp from date
+		airport.LastVisited = airport.LastVisited[0:10]
+
 		airports = append(airports, airport)
 	}
 
