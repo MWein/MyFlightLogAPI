893b24568aa3d8baaa4143ef9cc51649a85b5348
unknown <mweinberg21@gmail.com>
2020-07-21 20:33:34 -0500

    Timers

--- src/endpoints/planeImg.go
+++ src/endpoints/planeImg.go
@@ -4,9 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func PlaneImg(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
 	idents, ok := r.URL.Query()["ident"]
 
 	if !ok || len(idents[0]) < 1 {
@@ -24,17 +27,26 @@ func PlaneImg(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
+	elapsed := time.Since(start)
+	fmt.Printf("Connection took %s", elapsed)
+
+	start = time.Now()
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+	elapsed = time.Since(start)
+	fmt.Printf("Ping took %s", elapsed)
 
+	start = time.Now()
 	var image []byte
 	err = db.QueryRow("SELECT pic FROM plane WHERE ident = $1", idents[0]).Scan(&image)
 	if err != nil {
 		fmt.Fprintf(w, "Not Found")
 		return
 	}
+	elapsed = time.Since(start)
+	fmt.Printf("Getting image took %s", elapsed)
 
 	w.Header().Set("Content-Type", "image/jpeg")
 	w.Write(image)
