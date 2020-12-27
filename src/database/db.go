06a2749ae48a8c6ee13515f222de46d1ade03138
unknown <mweinberg21@gmail.com>
2020-12-26 22:17:40 -0600

    Moved thumbnail to own function for startup caching

--- src/database/db.go
+++ src/database/db.go
@@ -1,10 +1,14 @@
 package database
 
 import (
+	"bytes"
 	"database/sql"
 	"fmt"
+	"image"
+	"image/jpeg"
 	"time"
 
+	"github.com/disintegration/imaging"
 	"github.com/patrickmn/go-cache"
 )
 
@@ -33,4 +37,55 @@ func StartDB() {
 	Cache = cache.New(5*time.Minute, 10*time.Minute)
 
 	fmt.Println("Cache Ready")
+}
+
+func GetImage(imageId string, thumbnail bool) ([]byte, error) {
+	// Check cache first if thumbnail
+	if (thumbnail) {
+		cachedThumbnail, found := Cache.Get(imageId)
+		if found {
+			return cachedThumbnail.([]byte), nil
+		}
+	}
+
+	const imageQuery = `
+		SELECT data FROM (
+			SELECT id, data FROM pictures
+			UNION
+			SELECT id, data FROM build_log_picture
+			UNION
+			SELECT ident AS id, pic AS data FROM plane
+			UNION
+			SELECT id, cover AS data FROM build
+		) as allimages
+		WHERE id = $1
+	`
+
+	var imageData []byte
+	err := DBConnection.QueryRow(imageQuery, imageId).Scan(&imageData)
+	if err != nil {
+		var emptyBytes []byte
+		return emptyBytes, err
+	}
+
+	if !thumbnail {
+		return imageData, nil
+	}
+
+	// Create thumbnail image
+	// image.Image from bytes
+	img, _, _ := image.Decode(bytes.NewReader(imageData))
+	// Resize
+	dstImage := imaging.Resize(img, 0, 200, imaging.Lanczos)
+	// Back to []byte
+	buf := new(bytes.Buffer)
+	jpeg.Encode(buf, dstImage, nil)
+	bytes := buf.Bytes()
+
+	// Save to cache for next time
+	Cache.Set(imageId, bytes, cache.NoExpiration)
+
+	fmt.Printf("Set cache for %s\n", imageId)
+
+	return bytes, nil
 }
